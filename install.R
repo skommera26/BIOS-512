b24e796c0a5e2b24253118069e1592700956cdd1
skommera26 <kommer@ad.unc.edu>
2025-11-05 23:32:45 -0500

    Changes to install.R

--- install.R
+++ install.R
@@ -5,5 +5,8 @@ install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
 install.packages("skimr", dependencies = TRUE)
 install.packages("Rtsne", dependencies = TRUE)
+install.packages("glmnet", dependencies = TRUE)
+install.packages("broom", dependencies = TRUE)
+install.packages("gridExtra", dependencieds=TRUE)
 
 IRkernel::installspec(user = FALSE)
